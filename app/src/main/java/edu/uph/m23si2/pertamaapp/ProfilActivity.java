84d4cceb9d48bed45f006800038fc71d863b05a8
ade-m <66313509+ade-m@users.noreply.github.com>
2025-06-03 10:44:05 +0700

    Pertemuan 1-5

---
+++ app/src/main/java/edu/uph/m23si2/pertamaapp/ProfilActivity.java
@@ -0,0 +1,64 @@
+package edu.uph.m23si2.pertamaapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfilActivity extends AppCompatActivity {
+    Button btnSubmit;
+    EditText edtNama, edtEmail, edtProdi;
+    TextView txvHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profil);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        edtNama = findViewById(R.id.edtNama);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtProdi = findViewById(R.id.edtProdi);
+        txvHasil = findViewById(R.id.txvHasil);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Menampilkan hasil di txvHasil
+                String nama = edtNama.getText().toString();
+                String email = edtEmail.getText().toString();
+                String prodi = edtProdi.getText().toString();
+                txvHasil.setText(nama + "\n"+ email + "\nProgram Studi" + prodi + "\n" +
+                        getNamaFakultas(prodi));
+            }
+        });
+    }
+
+    String getNamaFakultas(String prodi){
+        String namaProdi = prodi.toLowerCase();
+        if(namaProdi.equals("sistem informasi") || namaProdi.equals("informatika")){
+            return "Fakultas Teknologi Informasi";
+        }
+        else if(namaProdi.equals("hukum") || namaProdi.equals("law")){
+            return "Fakultas Hukum";
+        }
+        else if(namaProdi.equals("akuntansi") || namaProdi.equals("manajemen")||
+                namaProdi.equals("perhotelan")){
+            return "Fakultas Ekonomi dan Bisnis";
+        }
+        else
+            return "Fakultas Tidak di Temukan";
+    }
+}
