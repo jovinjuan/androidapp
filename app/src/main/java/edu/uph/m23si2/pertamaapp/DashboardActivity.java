53719578bfa8a3e5f3a583d29379f54dc4337d6b
ade-m <66313509+ade-m@users.noreply.github.com>
2025-06-09 19:59:10 +0700

    Pertemuan 7

--- app/src/main/java/edu/uph/m23si2/pertamaapp/DashboardActivity.java
+++ app/src/main/java/edu/uph/m23si2/pertamaapp/DashboardActivity.java
@@ -2,17 +2,28 @@ package edu.uph.m23si2.pertamaapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
     LinearLayout llyProfil,llyTodolist;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +34,20 @@ public class DashboardActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         llyProfil= findViewById(R.id.llyProfil);
         llyTodolist=findViewById(R.id.llyTodolist);
@@ -48,6 +73,18 @@ public class DashboardActivity extends AppCompatActivity {
     public void toTodolist(){
         Intent intent = new Intent(this,TodoListActivity.class);
         startActivity(intent);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish(); // Menutup aktivitas saat tombol "Back" ditekan
     }
 
 }
